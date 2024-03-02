64147bc48ea7142ea963a07eb4ecf71b066bc8f1
Danilo Mendes <danilo.mirandha@gmail.com>
2024-03-02 10:39:57 -0300

    chore: refatorando para melhor organização

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDb from "./db/connectToMongoDb.js";
 
 const app = express();
-dotenv.config();
-
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
+dotenv.config();
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 

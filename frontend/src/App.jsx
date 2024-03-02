f324f23712a3748c76f060a66186a7ae7334e645
Danilo Mendes <danilo.mirandha@gmail.com>
2024-03-02 14:31:48 -0300

    feat: add página de login no app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
-    <>
-      <button className="btn btn-primary">Primary</button>
-    </>
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Login />
+    </div>
   );
 }
 

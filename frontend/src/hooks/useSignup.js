6de49bee8b841cf8867ad0f0a7bfab1401780f58
Danilo Mendes <danilo.mirandha@gmail.com>
2024-03-02 18:08:06 -0300

    feat: add cadastro do usuário no banco de dados

---
+++ frontend/src/hooks/useSignup.js
@@ -0,0 +1,76 @@
+import toast from "react-hot-toast";
+import { useState } from "react";
+
+export default function useSignup() {
+  const [loading, setLoading] = useState(false);
+
+  const signup = async ({
+    fullName,
+    username,
+    password,
+    confirmPassword,
+    gender,
+  }) => {
+    const success = handleInputErrors({
+      fullName,
+      username,
+      password,
+      confirmPassword,
+      gender,
+    });
+    if (!success) return;
+
+    setLoading(true);
+
+    try {
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          fullName,
+          username,
+          password,
+          confirmPassword,
+          gender,
+        }),
+      });
+
+      const data = await res.json();
+
+      if (data.error) {
+        throw new Error(data.error);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { loading, signup };
+}
+
+function handleInputErrors({
+  fullName,
+  username,
+  password,
+  confirmPassword,
+  gender,
+}) {
+  if (!fullName || !username || !password || !confirmPassword || !gender) {
+    toast.error("Por favor, preencha todos os campos!");
+    return false;
+  }
+
+  if (password !== confirmPassword) {
+    toast.error("As senhas não coincidem!");
+    return false;
+  }
+
+  if (password.length < 6) {
+    toast.error("A senha deve conter pelo menos 6 caracteres!");
+    return false;
+  }
+
+  return true;
+}
--- frontend/src/pages/signup/Signup.jsx
+++ frontend/src/pages/signup/Signup.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import { useState } from "react";
 import GenderCheckbox from "./GenderCheckbox";
 import { Link } from "react-router-dom";
+import useSignup from "../../hooks/useSignup";
 
 export default function SignUp() {
+  const [inputs, setInputs] = useState({
+    fullName: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+    gender: "",
+  });
+
+  const { loading, signup } = useSignup();
+
+  const handleCheckboxChange = (gender) => {
+    setInputs({ ...inputs, gender });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await signup(inputs);
+  };
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
@@ -10,7 +30,7 @@ export default function SignUp() {
           Cadastre-se
           <span className="text-blue-500"> ChatApp</span>
         </h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label className="label p-2">
               <span className="text-base label-text text-white">Nome</span>
@@ -19,6 +39,10 @@ export default function SignUp() {
               type="text"
               placeholder="Digite seu nome"
               className="w-full input input-bordered h-10"
+              value={inputs.fullName}
+              onChange={(e) =>
+                setInputs({ ...inputs, fullName: e.target.value })
+              }
             />
           </div>
           <div>
@@ -29,6 +53,10 @@ export default function SignUp() {
               type="text"
               placeholder="Digite o nome de usuário"
               className="w-full input input-bordered h-10"
+              value={inputs.username}
+              onChange={(e) =>
+                setInputs({ ...inputs, username: e.target.value })
+              }
             />
           </div>
           <div>
@@ -39,20 +67,33 @@ export default function SignUp() {
               type="password"
               placeholder="Digite sua senha"
               className="w-full input input-bordered h-10"
+              value={inputs.password}
+              onChange={(e) =>
+                setInputs({ ...inputs, password: e.target.value })
+              }
             />
           </div>
           <div>
             <label className="label p-2">
-              <span className="text-base label-text text-white">Confirmar senha</span>
+              <span className="text-base label-text text-white">
+                Confirmar senha
+              </span>
             </label>
             <input
               type="password"
               placeholder="confirme sua senha"
               className="w-full input input-bordered h-10"
+              value={inputs.confirmPassword}
+              onChange={(e) =>
+                setInputs({ ...inputs, confirmPassword: e.target.value })
+              }
             />
           </div>
 
-          <GenderCheckbox />
+          <GenderCheckbox
+            onCheckboxChange={handleCheckboxChange}
+            selectedGender={inputs.gender}
+          />
 
           <Link
             to="/login"
@@ -62,7 +103,9 @@ export default function SignUp() {
           </Link>
 
           <div>
-            <button className="btn btn-block btn-sm mt-2 border border-slate-700">Cadastrar</button>
+            <button className="btn btn-block btn-sm mt-2 border border-slate-700">
+              Cadastrar
+            </button>
           </div>
         </form>
       </div>

3d0924515c55ba7ec7443eb54e40ce04eb06c12b
Danilo Mendes <danilo.mirandha@gmail.com>
2024-03-02 11:00:22 -0300

    feat: add token jwt para gerar token e cookie para o usuário

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
   try {
@@ -30,7 +31,7 @@ export const signup = async (req, res) => {
     });
 
     if (newUser) {
-      
+      generateTokenAndSetCookie(newUser._id, res);
       await newUser.save();
 
       res.status(201).json({

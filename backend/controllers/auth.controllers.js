ebea0e22707ff4ffc54d332e55894c1ef1dde356
Danilo Mendes <danilo.mirandha@gmail.com>
2024-03-02 10:40:32 -0300

    feat: add autenticação do cadastro de usuário nos controller

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,14 +1,51 @@
+import User from "../models/user.model.js";
+
+export const signup = async (req, res) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "As senhas não coincidem." });
+    }
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({ error: "Este nome de usuário já existe." });
+    }
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      username: newUser.username,
+      password: newUser.password,
+      profilePic: newUser.profilePic,
+    });
+  } catch (error) {
+    console.log("Erro no signuo controller", error.message);
+    return res.status(500).json({ error: "Erro interno no servidor!" });
+  }
+};
+
 export const login = async (req, res) => {
   try {
   } catch (error) {}
 };
 
 export const logout = async (req, res) => {
-  try {
-  } catch (error) {}
-};
-
-export const signup = async (req, res) => {
   try {
   } catch (error) {}
 };

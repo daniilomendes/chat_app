76d6564fcf30f09aeab0317f71cb4ff49ccaf536
Danilo Mendes <danilo.mirandha@gmail.com>
2024-03-02 16:38:12 -0300

    feat: add compoents presentes na sidebar, barra de pesquisa, botão logout, conversar com outros usuários

---
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -0,0 +1,16 @@
+import { IoSearch } from "react-icons/io5";
+
+export default function SearchInput() {
+  return (
+    <form className="flex items-center gap-2">
+      <input
+        type="text"
+        placeholder="Pesquisar..."
+        className="input input-bordered rounded-full"
+      />
+      <button type="submit" className="btn btn-circle bg-sky-500 text-white">
+        <IoSearch className="w-6 h-6 outline-none" />
+      </button>
+    </form>
+  );
+}
